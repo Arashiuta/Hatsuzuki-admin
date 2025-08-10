81993df7440a2b388afb3841d0a582bc4723e6d1
Arashiuta <1209058433@qq.com>
2025-08-10 18:08:03 +0800

    update

--- src/components/global/hz-button/index.vue
+++ src/components/global/hz-button/index.vue
@@ -1,11 +1,11 @@
 <template>
-    <div ref="opButtonRef" class="opButton-container">
+    <div
+        :class="['opButton-container', { 'primary': props.type === 'primary' }, { 'normal': props.type === 'normal' }, { 'large': props.size === 'large' }, props.disabled ? 'disabled' : 'notDisabled']">
         <slot></slot>
     </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, watch } from 'vue';
 
 interface OpButtonProps {
     type?: 'primary' | 'normal';
@@ -18,45 +18,7 @@ const props = withDefaults(defineProps<OpButtonProps>(), {
     disabled: false,
 });
 
-const opButtonRef = ref<HTMLElement | null>(null);
 
-const initStyle = () => {
-    if (opButtonRef.value) {
-        if (props.type === 'primary') {
-            opButtonRef.value.style.backgroundColor = 'var(--hz-button-primary-background-color)';
-            opButtonRef.value.style.color = 'var(--hz-button-primary-text-color)';
-        }
-        if (props.type === 'normal') {
-            opButtonRef.value.style.border = '1px solid var(--hz-button-normal-border-color)';
-        }
-        if (props.size === 'large') {
-            opButtonRef.value.style.padding = '8px 50px';
-        }
-
-    }
-}
-
-watch(
-    () => props.disabled,
-    (disabled) => {
-        if (opButtonRef.value) {
-            if (disabled) {
-                opButtonRef.value.style.opacity = '0.5';
-                opButtonRef.value.style.pointerEvents = 'none';
-                opButtonRef.value.style.cursor = 'not-allowed';
-            } else {
-                opButtonRef.value.style.opacity = '1';
-                opButtonRef.value.style.pointerEvents = 'auto';
-                opButtonRef.value.style.cursor = 'pointer';
-            }
-        }
-    },
-    { immediate: true }
-);
-
-onMounted(() => {
-    initStyle()
-})
 </script>
 
 <style scoped lang="scss">
@@ -74,5 +36,30 @@ onMounted(() => {
     &:hover {
         filter: brightness(0.8);
     }
+}
+
+.primary {
+    background-color: var(--hz-button-primary-background-color);
+    color: var(--hz-button-primary-text-color);
+}
+
+.normal {
+    border: 1px solid var(--hz-button-normal-border-color)
+}
+
+.large {
+    padding: 8px 50px;
+}
+
+.disabled {
+    opacity: 0.5;
+    pointer-events: none;
+    cursor: not-allowed;
+}
+
+.notDisabled {
+    opacity: 1;
+    pointer-events: auto;
+    cursor: pointer;
 }
 </style>
